18f296000f38dd1ceb72926c92f234a69753520b
slofp <phy.public@gmail.com>
2024-08-18 13:11:12 +0900

    feat: add docker compose config

--- src/env.py
+++ src/env.py
@@ -1,6 +1,11 @@
 import os.path as osp
+from os import environ
 import dotenv.main
 
 dotenv_path = osp.join(osp.dirname(__file__), '.env')
-envs = dotenv.main.dotenv_values(dotenv_path)
 
+if 'IS_DOCKER' in environ:
+    dotenv.main.load_dotenv(dotenv_path)
+    envs = environ
+else:
+    envs = dotenv.main.dotenv_values(dotenv_path)
